e2eb842ede9136e02074d02196b02090332f7605
JuanZu77 <juanvzubiri77@gmail.com>
2024-03-13 22:23:26 -0300

    buscar categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,29 @@ public class CategoryRestController {
 	
 	@Autowired
 	private ICategoryService service;
+	
+	/**
+	 * Get all the Categories
+	 * @return
+	 */
 
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories(){
 		
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
+	}
+	
+	/**
+	 * Get categories by id
+	 * @param id
+	 * @return
+	 */
+	
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
 	}
 }
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,10 @@ public class CategoryServiceImpl implements ICategoryService{
 
 	@Autowired
 	private ICategoryDao categoryDao;
+	
+	/**
+	 * implements service get all the categories
+	 */
 	
 	@Override
 	@Transactional(readOnly = true)
@@ -36,6 +42,42 @@ public class CategoryServiceImpl implements ICategoryService{
 		}
 		
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+		
+	}
+
+	
+	
+	/**
+	 * implements service get Categories By Id
+	 */
+	
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+    CategoryResponseRest response = new CategoryResponseRest();
+    List<Category> list = new ArrayList<>();
+		
+		try {
+			 Optional<Category> category = categoryDao.findById(id);
+			 
+			 if(category.isPresent()) {
+				 list.add(category.get());
+				 response.getCategoryResponse().setCategory(list);
+				 response.setMetadata("Respuesta OK", "00", "Categoría Encontrada");
+			 }else {
+				 response.setMetadata("Error", "-1", "Categoria No encontrada");
+				 return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			 }
+		}
+		catch(Exception e) {
+			 response.setMetadata("Error", "-1", "Error al consultar");
+			 e.getStackTrace();
+			 return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+		
 		
 	}
 }
